077e83a60f10c83ba61b952fc0cb4646dca539d0
Neil Oribello <neil.oribello@gmail.com>
2021-02-03 05:22:30 +0000

    added without.js

---
+++ without.js
@@ -0,0 +1,56 @@
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+        console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+    } else {
+        console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+    }
+};
+
+// // TEST CODE
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+
+
+const eqArrays = function(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    for(let x = 0; x < arr1.length; x++) {
+        if(arr1[x] !== arr2[x]) {
+            return false
+        } 
+    } 
+    return true;
+}
+
+const arraysEqual = function(actual, expected) {
+    if (eqArrays(actual, expected)) {
+        console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+    } else {
+        console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+    }
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+//Implement without which will return a subset of a given array, removing unwanted elements.
+
+const without = function(source, itemsToRemove) {
+    let newArr = [];
+    for(let x = 0; x < source.length; x++) {
+        let item = false;
+        for(let y = 0; y < itemsToRemove.length; y++) {
+            if(source[x] === itemsToRemove[y]) {
+                item = true;
+            }
+        }
+        if(item === false) {
+            newArr.push(source[x])
+        }
+    }
+    return newArr;
+}
+
+
+console.log(without([1, 2, 3], [1])) // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"]) ) // => ["1", "2"]
