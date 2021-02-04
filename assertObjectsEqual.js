aab7302093c6f3744dfb3a6a677c99f5f0e355c7
Neil Oribello <neil.oribello@gmail.com>
2021-02-04 05:36:29 +0000

    adding assertObjectsEqual.js

---
+++ assertObjectsEqual.js
@@ -0,0 +1,27 @@
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+  if(Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+  for(const key of Object.keys(object1)) {
+    if(Array.isArray(object1[key]) && Array.isArray(object2[key])){
+      if (!eqArrays(object1[key],object2[key])){
+       return false;       
+      }
+    } 
+  }
+  return true;
+}
+
+// FUNCTION IMPLEMENTATION
+const assertObjectsEqual = function(actual, expected) {
+  // Implement me!
+  const inspect = require('util').inspect;
+  if (actual !== expected) {
+    console.log(`🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`)
+  } else {
+    console.log(`✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`)
+  }
+};
+
