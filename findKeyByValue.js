2a6a48fbe7d6ac121bb618f095bf4e6f99a0a8cb
Neil Oribello <neil.oribello@gmail.com>
2021-02-04 04:08:36 +0000

    adding rest of the files

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+      console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+      console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+const findKeyByValue = function(obj, val) {
+  for (const objKey in obj) {
+    if (obj[objKey] === val) {
+      return objKey;
+    }
+  }
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
