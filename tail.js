4956e32b7fd9d147bf650794b595991d04072e17
Neil Oribello <neil.oribello@gmail.com>
2021-02-08 21:03:18 +0000

    refactoring and adding tests for tail.js

--- tail.js
+++ tail.js
@@ -1,7 +1,3 @@
-const assertEqual = require('./assertEqual');
-// TEST CODE
-
-
 const tail = function(arr) {
   let tailArr = [];
   for (let x = 1; x < arr.length; x++) {
@@ -10,8 +6,6 @@ const tail = function(arr) {
   return tailArr;
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.export = tail;
 
 
