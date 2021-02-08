d290766e36518e38e8a3f9d62372bb4873757bbd
Neil Oribello <neil.oribello@gmail.com>
2021-02-08 21:31:03 +0000

    refactored so the eqArrays are required in the assertArraysEqual.js file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,29 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const eqArrays = require('./eqArrays');
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let x = 0; x < arr1.length; x++) {
-    if (arr1[x] !== arr2[x]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const arraysEqual = function(actual, expected) {
+const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
     console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -31,4 +8,5 @@ const arraysEqual = function(actual, expected) {
   }
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = assertArraysEqual;
+
