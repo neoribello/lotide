217f504708462ad3d8c8955dd50bc08240211ea0
Neil Oribello <neil.oribello@gmail.com>
2021-02-08 20:44:59 +0000

    updating head.js/adding headTest.js

--- head.js
+++ head.js
@@ -1,12 +1,5 @@
-const assertEqual = require('./assertEqual')
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
 const head = function(arr) {
   return arr[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
